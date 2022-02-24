343d756da3c655361e6c25ec30678fd8ce1a7972
peter.gu <gupingchang@126.com>
2022-02-24 06:02:21 -0800

    [C++PP][chap-8]:For template practise.

--- 8_chap_functions/template.cpp
+++ 8_chap_functions/template.cpp
@@ -1,7 +1,3 @@
-/*
- * Template makes it easy and secure to define the same function.
- * */
-
 #include <iostream>
 
 template <class Any> void Swap(Any &a,Any &b);
@@ -9,16 +5,19 @@ template <class Any> void Swap(Any &a,Any &b);
 int main()
 {
 	using namespace std;
-	int i = 10;
+
+    int i = 10;
 	int j = 20;
-	cout << "i , j ="<< i <<","<< j << "\n";
+
+    cout << "i , j ="<< i <<","<< j << "\n";
 	cout << "using compiler-generated int swapper: \n";
 	Swap(i,j);
 	cout << "Now i, j=" << i << ","<< j << ".\n";
 
 	double x = 24.5;
 	double y = 81.7;
-	cout << "x,y = "<< x << "," << y << ".\n";
+
+    cout << "x,y = "<< x << "," << y << ".\n";
 	cout << "Using compiler-generated double swapper: \n";
 	Swap(x,y);
 	cout << "Now x, y=" << x << ","<<y<<".\n";
@@ -33,3 +32,4 @@ template <class Any> void Swap(Any &a , Any &b)
 	a = b;
 	b = tmp;
 }
+
